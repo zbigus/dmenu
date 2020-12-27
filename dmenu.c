8efc34e55163c318041f4da91e7b1320d862547e
Zbigniew Tekiela <tekiela.zbigniew@gmail.com>
2020-12-27 02:18:53 +0100

    Patched json

--- dmenu.c
+++ dmenu.c
@@ -17,6 +17,7 @@
 #include <X11/extensions/Xinerama.h>
 #endif
 #include <X11/Xft/Xft.h>
+#include <jansson.h>
 
 #include "drw.h"
 #include "util.h"
@@ -34,6 +35,7 @@ struct item {
 	struct item *left, *right;
 	int out, hp;
 	double distance;
+	json_t *json;
 };
 
 static char **hpitems = NULL;
@@ -44,6 +46,8 @@ static int bh, mw, mh;
 static int inputw = 0, promptw;
 static int lrpad; /* sum of left and right padding */
 static size_t cursor;
+static size_t items_sz = 0;
+static size_t items_ln = 0;
 static struct item *items = NULL;
 static struct item *matches, *matchend;
 static struct item *prev, *curr, *next, *sel;
@@ -90,6 +94,18 @@ arrayhas(char **list, int length, char *item) {
 			return 1;
 	}
 	return 0;
+}
+
+static void listjson(json_t *obj);
+static json_t *json = NULL;
+
+static struct item *
+itemnew(void)
+{
+	if (items_ln + 1 >= (items_sz / sizeof *items))
+		if (!(items = realloc(items, (items_sz += BUFSIZ))))
+			die("cannot realloc %u bytes:", items_sz);
+	return &items[items_ln++];
 }
 
 static void
@@ -397,6 +413,8 @@ match(void)
 	size_t len, textsize;
 	struct item *item, *lhpprefix, *lprefix, *lsubstr, *hpprefixend, *prefixend, *substrend;
 
+	if (json)
+		fstrstr = strcasestr;
 	strcpy(buf, text);
 	/* separate input text into tokens to be matched individually */
 	for (s = strtok(buf, " "); s; tokv[tokc - 1] = s, s = strtok(NULL, " "))
@@ -671,7 +689,19 @@ insert:
 		break;
 	case XK_Return:
 	case XK_KP_Enter:
-		puts((sel && !(ev->state & ShiftMask)) ? sel->text : text);
+		if (sel && sel->json) {
+			if (json_is_object(sel->json)) {
+				listjson(sel->json);
+				text[0] = '\0';
+				match();
+				drawmenu();
+				break;
+			} else {
+				puts(json_string_value(sel->json));
+			}
+		} else {
+			puts((sel && !(ev->state & ShiftMask)) ? sel->text : text);
+		}
 		if (!(ev->state & ControlMask)) {
 			cleanup();
 			exit(0);
@@ -744,33 +774,72 @@ paste(void)
 }
 
 static void
+readjson(const char *path)
+{
+	json_error_t jerr;
+
+	if (!(json = json_load_file(path, 0, &jerr)))
+		die("%s @ line: %i - %s", jerr.text, jerr.line, path);
+}
+
+static void
+listjson(json_t *obj)
+{
+	void *iter;
+	unsigned imax = 0;
+	unsigned tmpmax = 0;
+	struct item *item;
+
+	items_ln = 0;
+	iter = json_object_iter(obj);
+	while (iter) {
+		item = itemnew();
+		item->text = (char*) json_object_iter_key(iter);
+		item->json = json_object_iter_value(iter);
+		item->out = 0;
+		drw_font_getexts(drw->fonts, item->text, strlen(item->text),
+				 &tmpmax, NULL);
+		if (tmpmax > inputw) {
+			inputw = tmpmax;
+			imax = items_ln - 1;
+		}
+		iter = json_object_iter_next(obj, iter);
+	}
+	if (items)
+		items[items_ln].text = NULL;
+	inputw = items ? TEXTW(items[imax].text) : 0;
+	lines = MIN(lines, items_ln - 1);
+}
+
+static void
 readstdin(void)
 {
 	char buf[sizeof text], *p;
-	size_t i, imax = 0, size = 0;
+	size_t i, size = 0;
+	unsigned int imax = 0;
 	unsigned int tmpmax = 0;
+	struct item *item;
 
 	/* read each line from stdin and add it to the item list */
 	for (i = 0; fgets(buf, sizeof buf, stdin); i++) {
-		if (i + 1 >= size / sizeof *items)
-			if (!(items = realloc(items, (size += BUFSIZ))))
-				die("cannot realloc %u bytes:", size);
+		item = itemnew();
 		if ((p = strchr(buf, '\n')))
 			*p = '\0';
-		if (!(items[i].text = strdup(buf)))
+		if (!(item->text = strdup(buf)))
 			die("cannot strdup %u bytes:", strlen(buf) + 1);
-		items[i].out = 0;
+		item->json = NULL;
+		item->out = 0;
 		items[i].hp = arrayhas(hpitems, hplength, items[i].text);
 		drw_font_getexts(drw->fonts, buf, strlen(buf), &tmpmax, NULL);
 		if (tmpmax > inputw) {
 			inputw = tmpmax;
-			imax = i;
+			imax = items_ln - 1;
 		}
 	}
 	if (items)
-		items[i].text = NULL;
+		items[items_ln].text = NULL;
 	inputw = items ? TEXTW(items[imax].text) : 0;
-	lines = MIN(lines, i);
+	lines = MIN(lines, items_ln);
 }
 
 static void
@@ -931,7 +1000,7 @@ setup(void)
 static void
 usage(void)
 {
-	fputs("usage: dmenu [-bfiv] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
+	fputs("usage: dmenu [-bfiv] [-j json-file] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
 	      "             [-nb color] [-nf color] [-sb color] [-sf color]\n"
 	      "             [-hb color] [-hf color] [-hp items]\n"
 	      "             [-nhb color] [-nhf color] [-shb color] [-shf color] [-w windowid]\n", stderr);
@@ -963,6 +1032,8 @@ main(int argc, char *argv[])
 		} else if (i + 1 == argc)
 			usage();
 		/* these options take one argument */
+		else if (!strcmp(argv[i], "-j"))
+			readjson(argv[++i]);
 		else if (!strcmp(argv[i], "-g")) {   /* number of columns in grid */
 			columns = atoi(argv[++i]);
 			if (lines == 0) lines = 1;
@@ -1027,9 +1098,15 @@ main(int argc, char *argv[])
 
 	if (fast && !isatty(0)) {
 		grabkeyboard();
-		readstdin();
+		if (json)
+			listjson(json);
+		else
+			readstdin();
 	} else {
-		readstdin();
+		if (json)
+			listjson(json);
+		else
+			readstdin();
 		grabkeyboard();
 	}
 	setup();
