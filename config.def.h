ae1da2e7ef770c1e2c3bf44a91c70b7e6f03ae07
Zbigniew Tekiela <tekiela.zbigniew@gmail.com>
2020-12-27 01:47:45 +0100

    Patched highpriority and added fuzzyfind compatibility

--- config.def.h
+++ config.def.h
@@ -18,6 +18,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 	[SchemeOutHighlight] = { "#ffc978", "#00ffff" },
+	[SchemeHp] = { "#bbbbbb", "#333333" }
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 0;
